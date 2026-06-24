5b2932c59a58f032cede695ae544746673304286
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2026-06-24 11:37:15 +0300

    Change the purpose of the application

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,6 +1,11 @@
-export default function Loading() {
+import { RingLoader } from "react-spinners";
 
+export default function Loading() {
     // Stack uses React Suspense, which will render this page while user data is being fetched.
     // See: https://nextjs.org/docs/app/api-reference/file-conventions/loading
-    return <></>;
+    return (
+        <div className="flex items-center justify-center min-h-screen bg-gray-50">
+            <RingLoader color="#4F46E5" size={150} className="mx-auto my-20" />
+        </div>
+    );
 }
