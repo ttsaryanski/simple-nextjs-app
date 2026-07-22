0793103149829b99738b36d45ee5d92d69e4dce5
Tsvetan Tsaryanski <ttsaryanski@gmail.com>
2026-07-22 17:34:00 +0300

    Test environments in production

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -5,7 +5,7 @@ import { SignIn } from "@clerk/nextjs";
 const SignInPage = () => {
     return (
         <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-purple-50 to-purple-100">
-            <div className="max-w-md w-full space-y-8">
+            <div className="login max-w-md w-full space-y-8 mt-6 mb-6">
                 <SignIn signUpUrl="/sign-up" />
             </div>
         </div>
--- app/sign-up/page.tsx
+++ app/sign-up/page.tsx
@@ -5,7 +5,7 @@ import { SignUp } from "@clerk/nextjs";
 const SignUpPage = () => {
     return (
         <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-purple-50 to-purple-100">
-            <div className="max-w-md w-full space-y-8">
+            <div className="login max-w-md w-full space-y-8 mt-6 mb-6">
                 <SignUp signInUrl="/sign-in" />
             </div>
         </div>
